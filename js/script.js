161664a6b51cfb3db38dbf51aa48f9fef350f770
Julien Liabeuf <julien@liabeuf.fr>
2016-05-14 14:16:10 +0700

    Remove Owl Carousel init script

--- js/script.js
+++ js/script.js
@@ -140,33 +140,7 @@ function parallax() {
       }
 
 	});
-
-/*----------------------------------------------------*/
-/*  Owl Carousel
-/*----------------------------------------------------*/
-
-
-    $(document).ready(function() {
      
-    $("#testimonial-slides").owlCarousel({
-     
-    navigation : false, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem:true
-     
-    // "singleItem:true" is a shortcut for:
-    // items : 1,
-    // itemsDesktop : false,
-    // itemsDesktopSmall : false,
-    // itemsTablet: false,
-    // itemsMobile : false
-     
-    });
-     
-    });
-
-
 /*----------------------------------------------------*/
 /*  Google Map
 ------------------------------------------------------*/
