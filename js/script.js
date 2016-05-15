1cea885acec76d35d2e6f922e54017a91ba5af86
Julien Liabeuf <julien@liabeuf.fr>
2016-05-15 12:19:28 +0700

    Simplify the Formspree script

--- js/script.js
+++ js/script.js
@@ -139,44 +139,31 @@ jQuery(document).ready(function ($) {
     /*	contact form
      ------------------------------------------------------*/
 
-    $('form#contactForm button.submit').click(function () {
-
-        $('#image-loader').fadeIn();
-
-        var contactName = $('#contactForm #contactName').val();
-        var contactEmail = $('#contactForm #contactEmail').val();
-        var contactSubject = $('#contactForm #contactSubject').val();
-        var contactMessage = $('#contactForm #contactMessage').val();
-
-        var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-            '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
+    var $contactForm = $('#contactForm');
+    $contactForm.submit(function(e) {
+        e.preventDefault();
         $.ajax({
-            url: "//formspree.io/vipada@srisawat.me",
-            method: "POST",
-            data: {name: contactName, email: contactEmail, _subject: contactSubject, message: contactMessage},
-            dataType: "json",
-            success: function (result) {
-
-                // Message was sent
-                if (jQuery.type(result) === "object" && "undefined" !== result.success && result.success == 'email sent') {
-                    $('#image-loader').fadeOut();
-                    $('#message-warning').hide();
-                    $('#contactForm').fadeOut();
-                    $('#message-success').fadeIn();
-                }
-                // There was an error
-                else {
-                    $('#image-loader').fadeOut();
-                    $('#message-warning').html(msg);
-                    $('#message-warning').fadeIn();
-                }
-
+            url: '//formspree.io/vipada@srisawat.me',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            beforeSend: function() {
+                $('#image-loader').fadeIn();
+            },
+            success: function(data) {
+                $('#image-loader').fadeOut();
+                $('#message-warning').hide();
+                $('#contactForm').fadeOut();
+                $('#message-success').fadeIn();
+            },
+            error: function(err) {
+                $('#image-loader').fadeOut();
+                $('#message-warning').html(msg);
+                $('#message-warning').fadeIn();
             }
-
         });
-        return false;
     });
+    
 });
 
 
